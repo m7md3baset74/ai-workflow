a794065df89c09c440733261908b4fef3b5ac48a
m7md3baset74 <mohamedabdelbasset265@gmail.com>
2026-01-03 18:48:17 +0200

    first commit

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,9 @@
+export default function BuilderPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">
+        Workflow Builder (Dragify Demo)
+      </h1>
+    </div>
+  );
+}
